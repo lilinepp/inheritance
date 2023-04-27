af7927cc441d59ae0f6a30bd60498df370ee31c1
lilinepp <129063961+lilinepp@users.noreply.github.com>
2023-04-27 20:08:53 +0900

    Update Lecturer.cpp

--- Lecturer.cpp
+++ Lecturer.cpp
@@ -1,14 +1,14 @@
-// Выполнила Мясоедова Алина Вмк-20
+// Р’С‹РїРѕР»РЅРёР»Р° РњСЏСЃРѕРµРґРѕРІР° РђР»РёРЅР° Р’РјРє-20
 #include <iostream>
 #include "Lecturer.h"
 using namespace std;
 
-// конструктор по умолчанию
+// РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
 Lecturer::Lecturer() 
-	:Human() {				//добавление составляющих базового класса
-	academic_degree = "-";					// нет учёной степени
-	name_university = "-";					// нет университета
-	num_hall = 0;						// номер аудитории
+	:Human() {				//РґРѕР±Р°РІР»РµРЅРёРµ СЃРѕСЃС‚Р°РІР»СЏСЋС‰РёС… Р±Р°Р·РѕРІРѕРіРѕ РєР»Р°СЃСЃР°
+	academic_degree = "-";					// РЅРµС‚ СѓС‡С‘РЅРѕР№ СЃС‚РµРїРµРЅРё
+	name_university = "-";					// РЅРµС‚ СѓРЅРёРІРµСЂСЃРёС‚РµС‚Р°
+	num_hall = 0;						// РЅРѕРјРµСЂ Р°СѓРґРёС‚РѕСЂРёРё
 }
 
 Lecturer::Lecturer(const string& _name, const string& _surname,
@@ -17,59 +17,59 @@ Lecturer::Lecturer(const string& _name, const string& _surname,
 	:Human(_name, _surname, _patronymic, _age,_weight) {
 
 
-	set_univ(_name_u);							// сеттер университета
-	set_hall(_number);						// сеттер аудитории
-	academic_degree = "-";					// нет учёной степени
+	set_univ(_name_u);							// СЃРµС‚С‚РµСЂ СѓРЅРёРІРµСЂСЃРёС‚РµС‚Р°
+	set_hall(_number);						// СЃРµС‚С‚РµСЂ Р°СѓРґРёС‚РѕСЂРёРё
+	academic_degree = "-";					// РЅРµС‚ СѓС‡С‘РЅРѕР№ СЃС‚РµРїРµРЅРё
 }
 
-void Lecturer::set_univ(const string& _name_u) {			// сеттер университета
-	if (!_name_u.empty()) {										// если строка не пустая,
-		name_university = _name_u;								// тогда записываем
+void Lecturer::set_univ(const string& _name_u) {			// СЃРµС‚С‚РµСЂ СѓРЅРёРІРµСЂСЃРёС‚РµС‚Р°
+	if (!_name_u.empty()) {										// РµСЃР»Рё СЃС‚СЂРѕРєР° РЅРµ РїСѓСЃС‚Р°СЏ,
+		name_university = _name_u;								// С‚РѕРіРґР° Р·Р°РїРёСЃС‹РІР°РµРј
 	}
 	else {
-		throw invalid_argument("Пустая строка университета!");
+		throw invalid_argument("РџСѓСЃС‚Р°СЏ СЃС‚СЂРѕРєР° СѓРЅРёРІРµСЂСЃРёС‚РµС‚Р°!");
 	}
 }
 
-void Lecturer::set_hall(const int& _number) {			// сеттер аудитории
-	if ((_number > 0) && (_number < 1000)) {		// если номер больше нуля и меньше 1000
-		num_hall = _number;						// тогда записываем
+void Lecturer::set_hall(const int& _number) {			// СЃРµС‚С‚РµСЂ Р°СѓРґРёС‚РѕСЂРёРё
+	if ((_number > 0) && (_number < 1000)) {		// РµСЃР»Рё РЅРѕРјРµСЂ Р±РѕР»СЊС€Рµ РЅСѓР»СЏ Рё РјРµРЅСЊС€Рµ 1000
+		num_hall = _number;						// С‚РѕРіРґР° Р·Р°РїРёСЃС‹РІР°РµРј
 	}
 	else {
-		throw  invalid_argument("Неверный номер кабинета!");
+		throw  invalid_argument("РќРµРІРµСЂРЅС‹Р№ РЅРѕРјРµСЂ РєР°Р±РёРЅРµС‚Р°!");
 	}
 }
 
-void Lecturer::acd_dgr_up() {						// присвоение учёной степени
+void Lecturer::acd_dgr_up() {						// РїСЂРёСЃРІРѕРµРЅРёРµ СѓС‡С‘РЅРѕР№ СЃС‚РµРїРµРЅРё
 	if (academic_degree == "-") {
-		academic_degree = "Кандидат наук";			// если степени нет, то кандидат наук
+		academic_degree = "РљР°РЅРґРёРґР°С‚ РЅР°СѓРє";			// РµСЃР»Рё СЃС‚РµРїРµРЅРё РЅРµС‚, С‚Рѕ РєР°РЅРґРёРґР°С‚ РЅР°СѓРє
 	}
-	else if (academic_degree == "Кандидат наук") {
-		academic_degree = "Доктор наук";			// если кандидат наук, то доктор наук
+	else if (academic_degree == "РљР°РЅРґРёРґР°С‚ РЅР°СѓРє") {
+		academic_degree = "Р”РѕРєС‚РѕСЂ РЅР°СѓРє";			// РµСЃР»Рё РєР°РЅРґРёРґР°С‚ РЅР°СѓРє, С‚Рѕ РґРѕРєС‚РѕСЂ РЅР°СѓРє
 	}
 }
 
-string Lecturer::get_acd_dgr() const {			// возврат учёной степени
+string Lecturer::get_acd_dgr() const {			// РІРѕР·РІСЂР°С‚ СѓС‡С‘РЅРѕР№ СЃС‚РµРїРµРЅРё
 	return academic_degree;
 }
 
-string Lecturer::get_univ() const {			// возврат университета
+string Lecturer::get_univ() const {			// РІРѕР·РІСЂР°С‚ СѓРЅРёРІРµСЂСЃРёС‚РµС‚Р°
 	return name_university;
 }
 
-int Lecturer::get_hall() const {					// возврат номера аудитории
+int Lecturer::get_hall() const {					// РІРѕР·РІСЂР°С‚ РЅРѕРјРµСЂР° Р°СѓРґРёС‚РѕСЂРёРё
 	return num_hall;
 }
 
-string Lecturer::get_string() const {			// формирование строки с данными через абзац
+string Lecturer::get_string() const {			// С„РѕСЂРјРёСЂРѕРІР°РЅРёРµ СЃС‚СЂРѕРєРё СЃ РґР°РЅРЅС‹РјРё С‡РµСЂРµР· Р°Р±Р·Р°С†
 
-	string full = "Имя: " + Human::get_name() + "\n"
-		+ "Фамилия: " + Human::get_surname() + "\n"
-		+ "Отчество: " + Human::get_patronymic() + "\n"
-		+ "Возраст: " + to_string(Human::get_age()) + "\n"
-		+ "Вес: " + to_string(Human::get_weight()) + "\n"
-		+ "Учёная степень: " + academic_degree + "\n"
-		+ "Университет: " + name_university + "\n"
-		+ "Номер аудитории: " + to_string(num_hall);
+	string full = "РРјСЏ: " + Human::get_name() + "\n"
+		+ "Р¤Р°РјРёР»РёСЏ: " + Human::get_surname() + "\n"
+		+ "РћС‚С‡РµСЃС‚РІРѕ: " + Human::get_patronymic() + "\n"
+		+ "Р’РѕР·СЂР°СЃС‚: " + to_string(Human::get_age()) + "\n"
+		+ "Р’РµСЃ: " + to_string(Human::get_weight()) + "\n"
+		+ "РЈС‡С‘РЅР°СЏ СЃС‚РµРїРµРЅСЊ: " + academic_degree + "\n"
+		+ "РЈРЅРёРІРµСЂСЃРёС‚РµС‚: " + name_university + "\n"
+		+ "РќРѕРјРµСЂ Р°СѓРґРёС‚РѕСЂРёРё: " + to_string(num_hall);
 	return full;
 }
