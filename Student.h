e8b3f25ff2b8f97529fa707a81ecfb0960dc231e
lilinepp <129063961+lilinepp@users.noreply.github.com>
2023-04-27 20:10:29 +0900

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,26 +1,26 @@
-// Выполнила Мясоедова Алина Вмк-20
+// Р’С‹РїРѕР»РЅРёР»Р° РњСЏСЃРѕРµРґРѕРІР° РђР»РёРЅР° Р’РјРє-20
 #pragma once
 #include <iostream>
 #include <string>
 #include "Human.h"
 using namespace std;
 
-class Student : public Human {					// класс Student с наследованием от класса Human 
+class Student : public Human {					// РєР»Р°СЃСЃ Student СЃ РЅР°СЃР»РµРґРѕРІР°РЅРёРµРј РѕС‚ РєР»Р°СЃСЃР° Human 
 private:
 	/// <summary>
-	///  номер зачётной книжки студента
+	///  РЅРѕРјРµСЂ Р·Р°С‡С‘С‚РЅРѕР№ РєРЅРёР¶РєРё СЃС‚СѓРґРµРЅС‚Р°
 	/// </summary>
 	int num_record;	
 	/// <summary>
-	/// номер курса студента
+	/// РЅРѕРјРµСЂ РєСѓСЂСЃР° СЃС‚СѓРґРµРЅС‚Р°
 	/// </summary>
 	int num_course;	
 	/// <summary>
-	/// название группы студента
+	/// РЅР°Р·РІР°РЅРёРµ РіСЂСѓРїРїС‹ СЃС‚СѓРґРµРЅС‚Р°
 	/// </summary>
 	string group;						
 	/// <summary>
-	/// текущий статус студента
+	/// С‚РµРєСѓС‰РёР№ СЃС‚Р°С‚СѓСЃ СЃС‚СѓРґРµРЅС‚Р°
 	/// </summary>
 	string status;							
 public:
@@ -29,16 +29,16 @@ public:
 		const int& _age, const int& _weight, const int& _num_rec, const int& _num_crs, 
 		const string& _group, const string& _status);
 
-	void set_num_rec(const int& _num_rec);					// ввод номера зачётной книжки
-	void set_num_crs(const int& _num_crs);					// ввод номера курса
-	void set_nums(const int& r, const int& c);			// ввод номеров зачётки и курса
-	void set_grp(const string& _group);					// ввод названия группы
-	void set_stat(const string& _status);				// ввод текущего статуса
-	int get_num_rec() const;							// возврат номера зачётки
-	int get_num_crs() const;							// возврат номера курса
-	string get_grp() const;						// возврат названия группы
-	string get_stat() const;						// возврат текущего статуса
-	void add_crs(const int& v);							// прибавление курса
-	void sub_crs(const int& v);							// уменьшение курса
-	string get_string() const;						// возврат строки с данными
+	void set_num_rec(const int& _num_rec);					// РІРІРѕРґ РЅРѕРјРµСЂР° Р·Р°С‡С‘С‚РЅРѕР№ РєРЅРёР¶РєРё
+	void set_num_crs(const int& _num_crs);					// РІРІРѕРґ РЅРѕРјРµСЂР° РєСѓСЂСЃР°
+	void set_nums(const int& r, const int& c);			// РІРІРѕРґ РЅРѕРјРµСЂРѕРІ Р·Р°С‡С‘С‚РєРё Рё РєСѓСЂСЃР°
+	void set_grp(const string& _group);					// РІРІРѕРґ РЅР°Р·РІР°РЅРёСЏ РіСЂСѓРїРїС‹
+	void set_stat(const string& _status);				// РІРІРѕРґ С‚РµРєСѓС‰РµРіРѕ СЃС‚Р°С‚СѓСЃР°
+	int get_num_rec() const;							// РІРѕР·РІСЂР°С‚ РЅРѕРјРµСЂР° Р·Р°С‡С‘С‚РєРё
+	int get_num_crs() const;							// РІРѕР·РІСЂР°С‚ РЅРѕРјРµСЂР° РєСѓСЂСЃР°
+	string get_grp() const;						// РІРѕР·РІСЂР°С‚ РЅР°Р·РІР°РЅРёСЏ РіСЂСѓРїРїС‹
+	string get_stat() const;						// РІРѕР·РІСЂР°С‚ С‚РµРєСѓС‰РµРіРѕ СЃС‚Р°С‚СѓСЃР°
+	void add_crs(const int& v);							// РїСЂРёР±Р°РІР»РµРЅРёРµ РєСѓСЂСЃР°
+	void sub_crs(const int& v);							// СѓРјРµРЅСЊС€РµРЅРёРµ РєСѓСЂСЃР°
+	string get_string() const;						// РІРѕР·РІСЂР°С‚ СЃС‚СЂРѕРєРё СЃ РґР°РЅРЅС‹РјРё
 }; 
