5fb327c69a965feb3f30b1ebe369a89a04529428
lilinepp <129063961+lilinepp@users.noreply.github.com>
2023-05-04 17:20:12 +0900

    Update Lecturer.h

--- Lecturer.h
+++ Lecturer.h
@@ -5,7 +5,7 @@
 #include "Human.h"
 using namespace std;
 
-class Lecturer : public Human { //класс унаследован от класса Human
+class Lecturer : public Human { //строим новый клас Lecturer на основе класса Human
 private:
 	/// <summary>
 	/// учебная степень
@@ -18,17 +18,23 @@ private:
 	/// <summary>
 	/// номер аудитории
 	/// </summary>
-	int num_hall;							
+	int num_hall;	
+	const string candidate_sciences="Кандидат наук"; //константные переменные для хранения данных о научной степени
+	const string doctor_sciences="Доктор наук";
+	const string no_sciences="Нет ученой степени";
 public:
 	Lecturer();
 	Lecturer(const string& _name, const string& _surname, const string& _patronymic,
-	const int& _age,const int& _weight, const string& _name_u, const int& _number);
+	const int _age,const string& _name_u, const int _number);
 
 	void set_univ(const string& _name_u);			// ввод университета
-	void set_hall(const int& _number);				// ввод номера аудитории
+	void set_hall(const int _number);				// ввод номера аудитории
 	void acd_dgr_up();								// присвоение новой учебной степени
 	string get_acd_dgr() const;				// возврат учёной степени
+    void acd_dgr_down();                          //убавление учетной степени
 	string get_univ() const;					// возврат университета
 	int get_hall() const;							// возврат номера аудитории
-	string get_string() const;					// возврат строки с данными
+	string tostring() const override;					// возврат строки с данными
 };
+//override-спецификатор,кот.нужен для того,чтобы компилятор проконтролировал 
+//правильную работу аналогичного метода в базовом классе и убедился,что метод базового класса виртуальный 
