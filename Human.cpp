f4275e1e651c7d655e9d80e8076c6066416db0c0
lilinepp <129063961+lilinepp@users.noreply.github.com>
2023-05-04 17:18:47 +0900

    Update Human.cpp

--- Human.cpp
+++ Human.cpp
@@ -1,141 +1,112 @@
 
-// Выполнила Мясоедова Алина Вмк-20
+// Р’С‹РїРѕР»РЅРёР»Р° РњСЏСЃРѕРµРґРѕРІР° РђР»РёРЅР° Р’РјРє-20
 #include <iostream>
 #include "Human.h"
 using namespace std;
 
-Human::Human() {								// конструктор по умолчанию
-	name = "-";							// отсутствие имени по умолчанию
-	surname = "-";								// отсутствие фамилии по умолчанию
-	patronymic = "-";							// отсутствие отчества по умолчанию
-	age = 0;									// нулевой возраст - человека не существует
-	weight = 0;                                 // нулевой вес - человека не существует
+Human::Human() {								// РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
+	name = "-";							// РѕС‚СЃСѓС‚СЃС‚РІРёРµ РёРјРµРЅРё РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
+	surname = "-";								// РѕС‚СЃСѓС‚СЃС‚РІРёРµ С„Р°РјРёР»РёРё РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
+	patronymic = "-";							// РѕС‚СЃСѓС‚СЃС‚РІРёРµ РѕС‚С‡РµСЃС‚РІР° РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
+	age = 0;									// РЅСѓР»РµРІРѕР№ РІРѕР·СЂР°СЃС‚ - С‡РµР»РѕРІРµРєР° РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚
+	
 }
 
-Human::Human(const string& _name, const string& _surname, const string& _patronymic, const int& _age, const int& _weight) {		// констуктор с входными данными
-	set_name(_name);								// сеттер для имени
-	set_surname(_surname);								// сеттер для фамилии
-	set_patronymic(_patronymic);								// сеттер для отчества
-	set_age(_age);									// сеттер для возраста
-	set_weight(_weight);
+Human::Human(const string& _name, const string& _surname, const string& _patronymic, const int _age) {		// РєРѕРЅСЃС‚СѓРєС‚РѕСЂ СЃ РІС…РѕРґРЅС‹РјРё РґР°РЅРЅС‹РјРё
+	set_name(_name);								// СЃРµС‚С‚РµСЂ РґР»СЏ РёРјРµРЅРё
+	set_surname(_surname);								// СЃРµС‚С‚РµСЂ РґР»СЏ С„Р°РјРёР»РёРё
+	set_patronymic(_patronymic);								// СЃРµС‚С‚РµСЂ РґР»СЏ РѕС‚С‡РµСЃС‚РІР°
+	set_age(_age);									// СЃРµС‚С‚РµСЂ РґР»СЏ РІРѕР·СЂР°СЃС‚
 }
 
-void Human::set_name(const string& _name) {	// сеттер для имени
-	if (!_name.empty()) {							// если строка имени не пустая,
-		name = _name;							// тогда имя присваивается
+void Human::set_name(const string& _name) {	// СЃРµС‚С‚РµСЂ РґР»СЏ РёРјРµРЅРё
+	if (!_name.empty()) {							// РµСЃР»Рё СЃС‚СЂРѕРєР° РёРјРµРЅРё РЅРµ РїСѓСЃС‚Р°СЏ,
+		name = _name;							// С‚РѕРіРґР° РёРјСЏ РїСЂРёСЃРІР°РёРІР°РµС‚СЃСЏ
 	}
 	else {
-		throw invalid_argument("Пустая строка имени!");
+		throw invalid_argument("РџСѓСЃС‚Р°СЏ СЃС‚СЂРѕРєР° РёРјРµРЅРё!");
 	}
 }
 
-void Human::set_surname(const string& _surname) {	// сеттер для фамилии
-	if (!_surname.empty()) {							// если строка фамилии не пустая,
-		surname = _surname;							// тогда фамилия присваивается
+void Human::set_surname(const string& _surname) {	// СЃРµС‚С‚РµСЂ РґР»СЏ С„Р°РјРёР»РёРё
+	if (!_surname.empty()) {							// РµСЃР»Рё СЃС‚СЂРѕРєР° С„Р°РјРёР»РёРё РЅРµ РїСѓСЃС‚Р°СЏ,
+		surname = _surname;							// С‚РѕРіРґР° С„Р°РјРёР»РёСЏ РїСЂРёСЃРІР°РёРІР°РµС‚СЃСЏ
 	}
 	else {
-		throw invalid_argument("Пустая строка фамилии!");
+		throw invalid_argument("РџСѓСЃС‚Р°СЏ СЃС‚СЂРѕРєР° С„Р°РјРёР»РёРё!");
 	}
 }
 
-void Human::set_patronymic(const string& _patronymic) {	// сеттер для отчества
-	if (!_patronymic.empty()) {							// если строка отчества не пустая,
-		patronymic = _patronymic;							// тогда отчество присваивается
+void Human::set_patronymic(const string& _patronymic) {	// СЃРµС‚С‚РµСЂ РґР»СЏ РѕС‚С‡РµСЃС‚РІР°
+	if (!_patronymic.empty()) {							// РµСЃР»Рё СЃС‚СЂРѕРєР° РѕС‚С‡РµСЃС‚РІР° РЅРµ РїСѓСЃС‚Р°СЏ,
+		patronymic = _patronymic;							// С‚РѕРіРґР° РѕС‚С‡РµСЃС‚РІРѕ РїСЂРёСЃРІР°РёРІР°РµС‚СЃСЏ
 	}
 	else {
-		throw invalid_argument("Пустая строка отчества!");
+		throw invalid_argument("РџСѓСЃС‚Р°СЏ СЃС‚СЂРѕРєР° РѕС‚С‡РµСЃС‚РІР°!");
 	}
 }
 
-void Human::set_fuln(const string& _name, const string& _surname, const string& _patronymic) {		// сеттер для инициалов
-	set_name(_name);								// сеттер для имени
-	set_surname(_surname);								// сеттер для фамилияя
-	set_patronymic(_patronymic);								// сеттер для отчества
+void Human::set_fuln(const string& _name, const string& _surname, const string& _patronymic) {		// СЃРµС‚С‚РµСЂ РґР»СЏ РёРЅРёС†РёР°Р»РѕРІ
+	set_name(_name);								// СЃРµС‚С‚РµСЂ РґР»СЏ РёРјРµРЅРё
+	set_surname(_surname);								// СЃРµС‚С‚РµСЂ РґР»СЏ С„Р°РјРёР»РёСЏСЏ
+	set_patronymic(_patronymic);								// СЃРµС‚С‚РµСЂ РґР»СЏ РѕС‚С‡РµСЃС‚РІР°
 }
 
-void Human::set_age(const int& _age) {				// сеттер для возраста
-	if ((_age >= 0) && (_age < 100)) {				// если возраст в диапазоне 0-100,
-		age = _age;								// тогда возраст присваивается
+void Human::set_age(const int _age) {				// СЃРµС‚С‚РµСЂ РґР»СЏ РІРѕР·СЂР°СЃС‚Р°
+	if ((_age >= 0) && (_age < 100)) {				// РµСЃР»Рё РІРѕР·СЂР°СЃС‚ РІ РґРёР°РїР°Р·РѕРЅРµ 0-100,
+		age = _age;								// С‚РѕРіРґР° РІРѕР·СЂР°СЃС‚ РїСЂРёСЃРІР°РёРІР°РµС‚СЃСЏ
 	}
 	else {
-		throw invalid_argument("Возраст не соответствует реальности!");
-	}
-};
-
-void Human::set_weight(const int& _weight) {				// сеттер для веса
-	if ((_weight >= 0) && (_weight < 250)) {				// если вес в диапазоне 0-250,
-		weight = _weight;								// тогда вес присваивается
-	}
-	else {
-		throw invalid_argument("Вес не соответствует реальности!");
+		throw invalid_argument("Р’РѕР·СЂР°СЃС‚ РЅРµ СЃРѕРѕС‚РІРµС‚СЃС‚РІСѓРµС‚ СЂРµР°Р»СЊРЅРѕСЃС‚Рё!");
 	}
 };
 
 
-string Human::get_name() const {				// возврат имени
+
+
+string Human::get_name() const {				// РІРѕР·РІСЂР°С‚ РёРјРµРЅРё
 	return name;
 };
 
-string Human::get_surname() const {				// возврат фамилии
+string Human::get_surname() const {				// РІРѕР·РІСЂР°С‚ С„Р°РјРёР»РёРё
 	return surname;
 };
 
-string Human::get_patronymic() const {				// возврат отчества
+string Human::get_patronymic() const {				// РІРѕР·РІСЂР°С‚ РѕС‚С‡РµСЃС‚РІР°
 	return patronymic;
 };
 
-int Human::get_age() const {						// возврат возраста
+int Human::get_age() const {						// РІРѕР·РІСЂР°С‚ РІРѕР·СЂР°СЃС‚Р°
 	return age;
 };
 
-int Human::get_weight() const {						// возврат возраста
-	return weight;
-};
 
-void Human::add_age(const int& _add_age) {					// прибавление к возрасту
-	if ((age + _add_age > 0) && (age + _add_age < 100)) {			// если прибавление не зайдёт за диапазон 0-100,
-		age += _add_age;									// тогда прибавляем
+void Human::add_age(const int _add_age) {					// РїСЂРёР±Р°РІР»РµРЅРёРµ Рє РІРѕР·СЂР°СЃС‚Сѓ
+	if ((age + _add_age > 0) && (age + _add_age < 100)) {			// РµСЃР»Рё РїСЂРёР±Р°РІР»РµРЅРёРµ РЅРµ Р·Р°Р№РґС‘С‚ Р·Р° РґРёР°РїР°Р·РѕРЅ 0-100,
+		age += _add_age;									// С‚РѕРіРґР° РїСЂРёР±Р°РІР»СЏРµРј
 	}
 	else {
-		throw invalid_argument("Прибавление выходит за границы теоретического возраста!");
+		throw invalid_argument("РџСЂРёР±Р°РІР»РµРЅРёРµ РІС‹С…РѕРґРёС‚ Р·Р° РіСЂР°РЅРёС†С‹ С‚РµРѕСЂРµС‚РёС‡РµСЃРєРѕРіРѕ РІРѕР·СЂР°СЃС‚Р°!");
 	}
 };
 
-void Human::sub_age(const int& _sub_age) {					// вычитание из возраста
-	if ((age - _sub_age > 0) && (age - _sub_age < 100)) {			// если вычитание не зайдёт за диапазон 0-100,
-		age -= _sub_age;									// тогда вычитаем
+void Human::sub_age(const int _sub_age) {					// РІС‹С‡РёС‚Р°РЅРёРµ РёР· РІРѕР·СЂР°СЃС‚Р°
+	if ((age - _sub_age > 0) && (age - _sub_age < 100)) {			// РµСЃР»Рё РІС‹С‡РёС‚Р°РЅРёРµ РЅРµ Р·Р°Р№РґС‘С‚ Р·Р° РґРёР°РїР°Р·РѕРЅ 0-100,
+		age -= _sub_age;									// С‚РѕРіРґР° РІС‹С‡РёС‚Р°РµРј
 	}
 	else {
-		throw invalid_argument("Вычитание выходит за границы теоретического возраста!");
-	}
-};
-
-void Human::add_weight(const int& _add_weight) {					// прибавление к весу
-	if ((weight + _add_weight > 0) && (weight + _add_weight < 250)) {			// если прибавление не зайдёт за диапазон 0-250,
-		weight += _add_weight;									// тогда прибавляем
-	}
-	else {
-		throw invalid_argument("Прибавление выходит за границы теоретического веса!");
-	}
-};
-
-void Human::sub_weight(const int& _sub_weight) {					// вычитание из веса
-	if ((weight - _sub_weight > 0) && (weight - _sub_weight < 250)) {			// если вычитание не зайдёт за диапазон 0-250,
-		weight -= _sub_weight;									// тогда вычитаем
-	}
-	else {
-		throw invalid_argument("Вычитание выходит за границы теоретического веса!");
+		throw invalid_argument("Р’С‹С‡РёС‚Р°РЅРёРµ РІС‹С…РѕРґРёС‚ Р·Р° РіСЂР°РЅРёС†С‹ С‚РµРѕСЂРµС‚РёС‡РµСЃРєРѕРіРѕ РІРѕР·СЂР°СЃС‚Р°!");
 	}
 };
 
 
-string Human::get_string() const {				// формирование строки с данными через абзац
 
-	string full = "Имя: " + name + "\n"
-		+ "Фамилия: " + surname + "\n"
-		+ "Отчество: " + patronymic + "\n"
-		+ "Возраст: " + to_string(age) + "\n"
-		+ "Вес: " + to_string(weight);
+string Human::tostring() const {				// С„РѕСЂРјРёСЂРѕРІР°РЅРёРµ СЃС‚СЂРѕРєРё СЃ РґР°РЅРЅС‹РјРё С‡РµСЂРµР· Р°Р±Р·Р°С†
+
+	string full = "РРјСЏ: " + name + "\n"
+		+ "Р¤Р°РјРёР»РёСЏ: " + surname + "\n"
+		+ "РћС‚С‡РµСЃС‚РІРѕ: " + patronymic + "\n"
+		+ "Р’РѕР·СЂР°СЃС‚: " + to_string(age);
 	return full;
 };
 
