71a58acebbd0efc82ef61dc926881c0534a2404a
lilinepp <129063961+lilinepp@users.noreply.github.com>
2023-06-18 13:49:11 +0900

    Update Lecturer.h

--- Lecturer.h
+++ Lecturer.h
@@ -18,23 +18,28 @@ private:
 	/// <summary>
 	/// номер аудитории
 	/// </summary>
+	
 	int num_hall;	
-	const string candidate_sciences="Кандидат наук"; //константные переменные для хранения данных о научной степени
-	const string doctor_sciences="Доктор наук";
-	const string no_sciences="Нет ученой степени";
+	
+	static const string candidate_sciences; //константные переменные для хранения данных о научной степени
+	static const string doctor_sciences;
+	static const string no_sciences;
+
 public:
 	Lecturer();
 	Lecturer(const string& _name, const string& _surname, const string& _patronymic,
-	const int _age,const string& _name_u, const int _number);
+	int _age,const string& _name_u,  int _number);
 
 	void set_univ(const string& _name_u);			// ввод университета
-	void set_hall(const int _number);				// ввод номера аудитории
+	void set_hall( int _number);				// ввод номера аудитории
 	void acd_dgr_up();								// присвоение новой учебной степени
 	string get_acd_dgr() const;				// возврат учёной степени
     void acd_dgr_down();                          //убавление учетной степени
 	string get_univ() const;					// возврат университета
 	int get_hall() const;							// возврат номера аудитории
-	string tostring() const override;					// возврат строки с данными
+	string tostring() override;					// возврат строки с данными
 };
 //override-спецификатор,кот.нужен для того,чтобы компилятор проконтролировал 
 //правильную работу аналогичного метода в базовом классе и убедился,что метод базового класса виртуальный 
+//Static - это ключевое слово в C++, используемое для придания элементу особых характеристик.
+//Для статических элементов выделение памяти происходит только один раз и существуют эти элементы до завершения программы. 
