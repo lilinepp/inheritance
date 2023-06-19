356c670f282de76a8c400c502d7f1c12ce3fc04d
lilinepp <129063961+lilinepp@users.noreply.github.com>
2023-06-19 19:21:29 +0900

    Update Class_Human.cpp

--- Class_Human.cpp
+++ Class_Human.cpp
@@ -24,36 +24,37 @@ int main() {
 
     
    
-  Human* entrant = new Human ("Петр", "Петров", "Петрович", 18);// динамическое создание
-  Student* freshman = new Student ("Сидр", "Сидоров", "Сидорович", 19,200000, 6, "ВМК-20", "Обучается");
+ // Human* entrant = new Human ("Петр", "Петров", "Петрович", 18);// динамическое создание
+  //Student* freshman = new Student ("Сидр", "Сидоров", "Сидорович", 19,200000, 4, "ВМК-20", "Обучается");
   Lecturer* teacher = new Lecturer ("Иван", "Иванов", "Иванович",20, "ЗабГУ", 203);
 
-  // freshman->add_age(0);                                                     // проверка прибавления к возрасту
-   // entrant->sub_age(0);                                                     // проверка вычитания из возраста
+   //freshman->add_age(10);                                                     // проверка прибавления к возрасту
+   //entrant->sub_age(0);                                                     // проверка вычитания из возраста
+   //freshman->sub_age(0); 
 
 
     //freshman->add_crs(1);                                                         // проверка увеличения курса
    // freshman->sub_crs(1);                                                         // проверка уменьшения курса
 
-   // teacher->acd_dgr_up();                                                   // проверка увеличения учёной степени до кандидата
-  //  teacher->acd_dgr_up();                                                   // проверка увеличения учёной степени до доктора
+    //teacher->acd_dgr_up();                                                   // проверка увеличения учёной степени до кандидата
+   //teacher->acd_dgr_up();                                                   // проверка увеличения учёной степени до доктора
 
-   // teacher->acd_dgr_down();
+  // teacher->acd_dgr_down();                                                // проверка уменьшения учёной степени до доктора
+  // teacher->acd_dgr_down();                                                // проверка уменьшения учёной степени до доктора
 
-
-    cout << "Абитуриент:\n" << entrant->tostring() << endl << endl;          // вывод получившегося объекта Human
-    cout << "Cтудент:\n" << freshman->tostring() << endl << endl;          // вывод получившегося объекта Student
-    cout << "Преподаватель:\n" << teacher->tostring() << endl << endl;                     // вывод получившегося объекта Lecturer
+   // cout << "Абитуриент:\n" << entrant->tostring() << endl << endl;          // вывод получившегося объекта Human
+    //cout << "Cтудент:\n" << freshman->tostring() << endl << endl;          // вывод получившегося объекта Student
+   cout << "Преподаватель:\n" << teacher->tostring() << endl << endl;                     // вывод получившегося объекта Lecturer
 
    //delete entrant;                                                          // очистка памяти от Human
-   //delete freshman;                                                          // очистка памяти от Student
-  // delete teacher;                                                              // очистка памяти от Lecturer
+  // delete freshman;                                                          // очистка памяти от Student
+   delete teacher;                                                              // очистка памяти от Lecturer
 
   //демонстрация работы виртуального метода
    
-   Lecturer dmnstr("Мария", "Иванова", "Алексеевна", 20, "НГУ", 656);//создали объект наследуемого класса
-   Human* indicator = &dmnstr;//присвоили базовому классу указатель на объект наследника
-   cout << indicator->tostring()<<endl<<endl;//с помощью виртуального метода базового класса вывели объект наследуемого класса 
+  Lecturer dmnstr("Мария", "Иванова", "Алексеевна", 20, "НГУ", 656);//создали объект наследуемого класса
+  Human* indicator = &dmnstr;//присвоили базовому классу указатель на объект наследника
+  cout << indicator->tostring()<<endl<<endl;//с помощью виртуального метода базового класса вывели объект наследуемого класса 
 
     
 
